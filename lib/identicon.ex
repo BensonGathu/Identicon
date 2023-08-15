774fd6918a240bd40c6a1018d62fba46b6ca9818
Benson gathu <bensongathu23@gmail.com>
2023-08-15 10:16:47 +0300

    progress

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,13 +5,51 @@ defmodule Identicon do
   def main(input) do
     input
     |> harsh_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
   end
 
   def harsh_input(input) do
     # hash = :crypto.hash(:md5, input)
     # :binary.bin_to_list(hash)
 
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(image) do
+    %Identicon.Image{grid: grid} = image
+    grid = Enum.filter(grid, fn {code, _index} -> rem(code, 2) == 0 end)
+
+    %Identicon.Image{image | grid: grid}
   end
 end
